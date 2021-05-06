38041d2de594e0f9f34f0939d50197663ec3359a
edelmutic <edelmutic@gmail.com>
2021-05-06 13:59:59 +0300

    init project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://qwerty:qwerty123@cluster0.lqroj.mongodb.net/auth_roles?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    app.get('/', (req, res) => res.send('Hello there!'));
+    app.listen(PORT, () => console.log(`🚀 Server Started on ${PORT}!`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
