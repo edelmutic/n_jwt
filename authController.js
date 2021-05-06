1d6fd666e06681aed58eb6693f679ae46bf2f26d
edelmutic <edelmutic@gmail.com>
2021-05-06 16:44:36 +0300

    update controller

--- authController.js
+++ authController.js
@@ -1,11 +1,26 @@
+const Role = require('./models/Role');
+const User = require('./models/User');
+
 class authController {
   async registration(req, res) {
     try {
-    } catch (error) {}
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res.status(400).json({ message: 'User already exist' });
+      }
+      const user = new User();
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: 'Registration error' });
+    }
   }
   async login(req, res) {
     try {
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: 'Login error' });
+    }
   }
   async getUsers(req, res) {
     try {
