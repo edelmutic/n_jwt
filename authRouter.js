3b0e666267be25196124dd96c0b3a140ba50dffd
edelmutic <edelmutic@gmail.com>
2021-05-07 11:43:50 +0300

    add validator

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,18 @@
 const Router = require('express');
 const controller = require('./authController');
 const router = new Router();
+const { check } = require('express-validator');
 
-router.post('/registration', controller.registration);
+router.post(
+  '/registration',
+  [
+    check('username', 'Username can not be empty').notEmpty(),
+    check('password', 'Password shoud be more than 4 symbols').isLength({
+      min: 4,
+    }),
+  ],
+  controller.registration
+);
 router.post('/login', controller.login);
 router.get('/users', controller.getUsers);
 
