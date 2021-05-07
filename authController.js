09d65696a1758ede6573b9843f4598ca032d124b
edelmutic <edelmutic@gmail.com>
2021-05-07 11:49:42 +0300

    handle validation error

--- authController.js
+++ authController.js
@@ -1,10 +1,16 @@
 const Role = require('./models/Role');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const { json } = require('express');
 
 class authController {
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Registration error', errors });
+      }
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
       if (candidate) {
